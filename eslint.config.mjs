e0cf2c9493fcd1cb5426c3e7bb7b49d99569b421
Shaikh6767 <85774746+mumair738@users.noreply.github.com>
2025-12-01 16:40:15 +0530

    Update eslint.config.mjs
    globalIgnores is misplaced

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,19 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig } from "eslint/config";
+import next from "eslint-config-next";
+import tseslint from "typescript-eslint";
+import { globalIgnores } from "eslint/config";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+export default defineConfig([
+  {
+    ignores: [
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+  },
+
+  ...next(),
+  ...tseslint.configs.recommendedTypeCheckedOnly,
+  ...tseslint.configs.stylisticTypeChecked,
 ]);
-
-export default eslintConfig;
