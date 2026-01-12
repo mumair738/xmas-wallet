c486c2af4e65b6c9d6c44a6c4bce760bc9f65a62
Shaikh6767 <85774746+mumair738@users.noreply.github.com>
2026-01-12 16:54:24 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect } from "reac";
 
 export default function XmasWalletApp() {
   const [ready, setReady] = useState(false);
